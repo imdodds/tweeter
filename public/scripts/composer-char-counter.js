a9a8775a77ec38598f2398489c241f6823ce764c
Ian Dodds <ianmitchelldodds@gmail.com>
2022-09-13 21:42:55 +0000

    Add tweet container and hover states

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-  // --- our code goes here ---
 
-  console.log("Ready!");
+  // console.log("Ready!");
 
   const tweetText = document.getElementById('tweet-text');
 
@@ -9,21 +8,17 @@ $(document).ready(function() {
 
     const charsLeft = (140 - this.value.length);
 
-    // console.log(event);
-    // console.log(event.target.value.length);
-    // console.log("this:", this);
-
     const counter = $(this).parent().find('.counter');
     counter[0].innerHTML = charsLeft;
 
-    //console.log("this.counter:", $(this).filter("counter"));
-    // console.log("Characters left:", charsLeft);
-
     if (charsLeft < 0) {
-      counter.addClass('redCounter');
-    }    
+      counter.addClass('red-counter');
+    }
+
+    if (charsLeft >= 0) {
+      counter.removeClass('red-counter');
+    }
 
   });
 
 });
-
